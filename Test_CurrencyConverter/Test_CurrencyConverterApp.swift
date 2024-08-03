18b81414775224be77847d2790f91a82b6e1b9ef
Nazar <nazar.tsap2349@gmail.com>
2024-08-03 12:32:16 +0300

    Initial Commit

---
+++ Test_CurrencyConverter/Test_CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_CurrencyConverterApp.swift
+//  Test_CurrencyConverter
+//
+//  Created by назар Цап on 03.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
